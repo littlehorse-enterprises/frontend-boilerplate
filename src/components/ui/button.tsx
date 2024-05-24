3e55ff17621b64b52e6cb658118277ab8c81af59
Hazim Arafa <hazimarafa80@gmail.com>
2024-05-24 14:31:18 -0700

    feat: loading prop on button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { Loader2Icon } from "lucide-react";
 
 const buttonVariants = cva(
 	"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -38,21 +39,43 @@ export interface ButtonProps
 	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
 		VariantProps<typeof buttonVariants> {
 	asChild?: boolean;
+	loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, size, asChild = false, ...props }, ref) => {
+	(
+		{
+			children,
+			className,
+			variant,
+			size,
+			loading = false,
+			asChild = false,
+			...props
+		},
+		ref
+	) => {
 		const Comp = asChild ? Slot : "button";
 		return (
 			<Comp
 				className={cn(buttonVariants({ variant, size, className }))}
 				ref={ref}
 				{...props}
-			/>
+			>
+				{loading ? (
+					<Loader2Icon
+						className={cn(
+							"h-4 w-4 animate-spin",
+							children && "mr-2"
+						)}
+					/>
+				) : (
+					children
+				)}
+			</Comp>
 		);
 	}
 );
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
-
